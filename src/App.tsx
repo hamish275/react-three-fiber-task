df29ebaa16709ffdc081b60644923fffbb038574
Hamish <hamishjury96@gmail.com>
2022-01-06 16:12:16 +0100

    add orbit controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import './App.css';
 import React from 'react';
-import { Canvas } from "@react-three/fiber";
+import {Canvas, useThree, extend, ReactThreeFiber} from "@react-three/fiber";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+extend({OrbitControls});
+
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            'orbitControls': ReactThreeFiber.Object3DNode<OrbitControls, typeof OrbitControls>;
+        }
+    }
+}
 
 function App() {
   return (
@@ -9,9 +19,20 @@ function App() {
               <boxBufferGeometry args={[10, 10, 10]}/>
               <meshPhysicalMaterial wireframe={true}/>
           </mesh>
-          <axesHelper args={[30, 30]}/>
+          <axesHelper args={[30]}/>
+          <CameraControls/>
       </Canvas>
   )
+}
+
+function CameraControls(){
+    const {
+        camera,
+        gl: { domElement },
+    } = useThree();
+    return (
+        <orbitControls args={[camera, domElement]}/>
+    )
 }
 
 export default App;
